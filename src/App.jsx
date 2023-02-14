1a32a63d0c4c703fa6ac58d6b17108abe9b0ed73
Mohamed Rasheed S <mrasheed.fsd@gmail.com>
2023-02-14 13:18:00 +0530

    first react commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./component/header.jsx";
+import Note from "./component/note.jsx";
+import Footer from "./component/footer.jsx";
+
+function App() {
+  return (
+    <div>
+      <Header />;
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/note.jsx
@@ -0,0 +1,8 @@
+export default function Note() {
+  return (
+    <div className="note">
+      <h1>this is for title</h1>
+      <p>this is for content</p>
+    </div>
+  );
+}
